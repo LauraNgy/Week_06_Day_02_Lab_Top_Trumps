a574039761d77e61bee00c46b1336ea434940a50
Laura Nagy <ngylaura@gmail.com>
2018-09-04 23:33:31 +0100

    tests 'Game' constructor

--- models/game.js
+++ models/game.js
@@ -4,11 +4,23 @@ const Game = function (players, cards) {
 };
 
 Game.prototype.dealCards = function () {
-
+  for (let i = 0; i < this.cards.length; i++){
+    if (i % 2 === 0) {
+      this.players[0].handOfCards.push(this.cards[i]);
+    }
+    else this.players[1].handOfCards.push(this.cards[i]);
+  }
 };
 
-Game.prototype.playTurn = function () {
-
+Game.prototype.playTurn = function (player1, player2) {
+  let winner = player1.name;
+  const card1 = player1.playCard();
+  const card2 = player2.playCard();
+  const turnCat = player1.selectCategory(card1);
+  if (card1.category[turnCat] < card2.category[turnCat]) {
+    winner = player2.name;
+  }
+  return `The winner is ${winner}`
 };
 
 module.exports = Game;
--- models/specs/game_spec.js
+++ models/specs/game_spec.js
@@ -10,34 +10,64 @@ let card1;
 let card2;
 let card3;
 let card4;
+let card5;
+let card6;
 let category1;
 let category2;
 let category3;
 let category4;
+let category5;
+let category6;
 let game;
 
 beforeEach( function () {
   category1 = new Category(6, 9, 7);
   category2 = new Category(7, 10, 5);
   category3 = new Category(8, 6, 9);
+  category4 = new Category(7, 4, 10);
+  category5 = new Category(8, 7, 5);
+  category6 = new Category(10, 5, 6)
   card1 = new Card("Superman", category1);
   card2 = new Card("Scarlet Witch", category2);
   card3 = new Card("Black Widow", category3);
+  card4 = new Card("The Flash", category4);
+  card5 = new Card("Wonder Woman", category5);
+  card6 = new Card("Batman", category6)
   player1 = new Player("Sharon", []);
   player2 = new Player("Amy", []);
   const players = [player1, player2];
-  const cards = [card1, card2, card3, card4];
+  const cards = [card1, card2, card3, card4, card5, card6];
   game = new Game(players, cards)
 });
 
 describe("Game", function () {
 
-  it("should have players");
+  it("should have players", function () {
+    const actual = game.players;
+    assert.deepStrictEqual(actual, [player1, player2]);
+  });
 
-  it("should have cards");
+  it("should have cards", function () {
+    const actual = game.cards;
+    assert.deepStrictEqual(actual, [card1, card2, card3, card4, card5, card6]);
+  });
 
-  it("should deal cards");
+  it("should deal cards -- player1", function () {
+    game.dealCards();
+    const actual = player1.handOfCards;
+    assert.deepStrictEqual(actual, [card1, card3, card5]);
+  });
 
-  it("should play turn");
-  
+  it("should deal cards -- player2", function () {
+    game.dealCards();
+    const actual = player2.handOfCards;
+    assert.deepStrictEqual(actual, [card2, card4, card6]);
+  });
+
+  it("should play turn", function () {
+    game.dealCards();
+    const actual = game.playTurn(player1, player2);
+    assert.strictEqual(actual, "The winner is Amy")
+  });
+
 });
