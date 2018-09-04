7ff970d9e68a3bd6f1df5820953f36436268382b
Laura Nagy <ngylaura@gmail.com>
2018-09-04 16:16:11 +0100

    initial commit

---
+++ models/game.js
@@ -0,0 +1,14 @@
+const Game = function (players, cards) {
+  this.players = [];
+  this.cards = [];
+};
+
+Game.prototype.dealCards = function () {
+
+};
+
+Game.prototype.play = function () {
+
+};
+
+module.exports = Game;
---
+++ models/player.js
@@ -0,0 +1,14 @@
+const Player = function (name, handOfCards) {
+  this.name = name;
+  this.handOfCards = handOfCards;
+};
+
+Player.prototype.playCard = function () {
+
+};
+
+Player.prototype.selectCategory = function () {
+
+};
+
+module.exports = Player;
